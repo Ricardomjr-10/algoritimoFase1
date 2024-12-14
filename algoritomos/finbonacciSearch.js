fec25c0fe8f829ca7ce106fecbd2409f88344b7c
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-13 23:31:19 -0300

    build:fibonacci

--- algoritomos/finbonacciSearch.js
+++ algoritomos/finbonacciSearch.js
@@ -1,11 +1,18 @@
 // calculo dos numeros da fibonacci
 let fibMinus2 = 0
 let fibMinus1 = 1
-let fib
+let fib = 0
+let array = []
 let n = array.length
+n = 5
 
-while (fib <= n) {
+
+ while (fib <= n) {
     fib = fibMinus2 + fibMinus1
     fibMinus2 = fibMinus1
     fibMinus1 = fib
 }
+
+let indLeft = 0
+let indRight = n -1
+console.log(fib)
