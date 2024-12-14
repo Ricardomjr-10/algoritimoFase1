cb3e6d1eec036f56b664e76af46f4f5fcd868975
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-14 16:42:45 -0300

    fix

--- algoritomos/selectionSort.js
+++ algoritomos/selectionSort.js
@@ -2,15 +2,15 @@ const array = [64, 25, 12, 22, 11]
 
 
 for (let i = 0; i < array.length - 1; i++) {
-    let minimo = Math.min(array[i])
+    let minimo = i
     for (let j = i + 1; j < array.length; j++) {
-        if (array[j] < minimo) {
-            minimo = array[j]
+        if (array[j] < array[minimo]) {
+            minimo = j
         }
     }
     let temp = array[i]
-    array[i] = minimo
-    minimo = temp
+    array[i] = array[minimo]
+    array[minimo] = temp
 }
 
 console.log(array)
