9f80788c9d9610f8efb68fe5feaac54e9e78d389
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-14 16:39:39 -0300

    bug

--- algoritomos/selectionSort.js
+++ algoritomos/selectionSort.js
@@ -1,5 +1,16 @@
-const array  = [64, 25, 12, 22, 11]
+const array = [64, 25, 12, 22, 11]
 
-let minimo = array.Math.min(array)
 
-console.log(minimo)
+for (let i = 0; i < array.length - 1; i++) {
+    let minimo = Math.min(array[i])
+    for (let j = i + 1; j < array.length; j++) {
+        if (array[j] < minimo) {
+            minimo = array[j]
+        }
+    }
+    let temp = array[i]
+    array[i] = minimo
+    minimo = temp
+}
+
+console.log(array)
