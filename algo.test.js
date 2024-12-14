a22ab4fc6215f1a42f7791a266f8489af0fd61b0
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-14 17:08:56 -0300

    testando depuracao

--- algo.test.js
+++ algo.test.js
@@ -1,6 +1,7 @@
 
 const missingNumber = require('./algoritomos/missingNumber.js')
 const jumpSearch = require('./algoritomos/jumpSearch.js')
+const selectionSort = require('./algoritomos/selectionSort.js')
 
 test('Missing number', () => {
     expect(missingNumber([1, 2, 4, 6, 3, 7, 8])).toBe(5)
@@ -8,4 +9,8 @@ test('Missing number', () => {
 
 test('Jump Search', () => {
     expect(jumpSearch([0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610], 55)).toBe(10)
+})
+
+test('Selection Sort', () => {
+    expect(selectionSort([64, 25, 12, 22, 11])).toEqual([11, 12, 22, 25, 64])
 })
