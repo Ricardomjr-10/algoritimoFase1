14fc23cd986e71918143d14a8b8f545cecc077a6
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-15 12:23:23 -0300

    comecando a funcao

--- algoritimos/mergeSort.js
+++ algoritimos/mergeSort.js
@@ -1,18 +1,23 @@
 const array = [38, 27, 43, 10]
-let pHalf = array.slice(0, 2)
-let sHalf = array.slice(2, 4)
 
-let pp1Half = pHalf.slice(0, 1)
-let pp2Half = pHalf.slice(1)
-let ss1Half = sHalf.slice(0, 1)
-let ss2Half = sHalf.slice(1)
+const mergeSort = array => {
+    let pHalf = array.slice(0, 2)
+    let sHalf = array.slice(2, 4)
+    
+    let pp1Half = pHalf.slice(0, 1)
+    let pp2Half = pHalf.slice(1)
+    let ss1Half = sHalf.slice(0, 1)
+    let ss2Half = sHalf.slice(1)
+    
+    let Pmetade = ss2Half.concat(ss1Half)
+    let Smetade = pp2Half.concat(pp1Half)
+    
+    let metadeSort = Smetade.concat(Pmetade).sort((a, b) => a - b)
 
-let Pmetade = ss2Half.concat(ss1Half)
-let Smetade = pp2Half.concat(pp1Half)
+    return metadeSort
+}
 
-let metadeSort = Smetade.concat(Pmetade).sort((a, b) => a - b)
-
-console.log(metadeSort)
+console.log(mergeSort(array))
 
 // console.log(pHalf)
 // console.log(sHalf)
