40b94735f1b75658d547418e849f783f9fdabfaa
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-15 12:20:25 -0300

    juntando e colocando em ordem

--- algoritimos/mergeSort.js
+++ algoritimos/mergeSort.js
@@ -7,9 +7,16 @@ let pp2Half = pHalf.slice(1)
 let ss1Half = sHalf.slice(0, 1)
 let ss2Half = sHalf.slice(1)
 
-console.log(pHalf)
-console.log(sHalf)
-console.log(pp1Half)
-console.log(pp2Half)
-console.log(ss1Half)
-console.log(ss2Half)
+let Pmetade = ss2Half.concat(ss1Half)
+let Smetade = pp2Half.concat(pp1Half)
+
+let metadeSort = Smetade.concat(Pmetade).sort((a, b) => a - b)
+
+console.log(metadeSort)
+
+// console.log(pHalf)
+// console.log(sHalf)
+// console.log(pp1Half)
+// console.log(pp2Half)
+// console.log(ss1Half)
+// console.log(ss2Half)
