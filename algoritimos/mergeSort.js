17811556246ac16d843c573c6edddfe282e47cbf
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-15 12:10:01 -0300

    dividindo o array

--- algoritimos/mergeSort.js
+++ algoritimos/mergeSort.js
@@ -2,5 +2,14 @@ const array = [38, 27, 43, 10]
 let pHalf = array.slice(0, 2)
 let sHalf = array.slice(2, 4)
 
+let pp1Half = pHalf.slice(0, 1)
+let pp2Half = pHalf.slice(1)
+let ss1Half = sHalf.slice(0, 1)
+let ss2Half = sHalf.slice(1)
+
 console.log(pHalf)
 console.log(sHalf)
+console.log(pp1Half)
+console.log(pp2Half)
+console.log(ss1Half)
+console.log(ss2Half)
