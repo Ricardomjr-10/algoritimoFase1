02ae2c082f9da2ab1440f8d46d2a31fe087e3140
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-16 09:34:03 -0300

    chose k

--- algoritimos/minimumSumProduct.js
+++ algoritimos/minimumSumProduct.js
@@ -1,6 +1,10 @@
 const arrA = [1, 2, 3]
 const arrB = [4, 5, 6]
+let arrC = []
+let sum = 0
 
 for (let i = 0; i < arrA.length; i++) {
-    console.log(arrA[i] * arrB[i])
+    arrC.push(arrA[i] * arrB[i])
+    sum += arrC[i]
 }
+console.log(sum)
