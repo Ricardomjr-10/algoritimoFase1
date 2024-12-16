39f2dedf64cc8100b079cafac8d5f484f88ae979
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-16 11:11:04 -0300

    refactor function

--- algoritimos/minimumSumProduct.js
+++ algoritimos/minimumSumProduct.js
@@ -1,23 +1,21 @@
 const arrA = [1, 2, 3]
 const arrB = [4, 5, 6]
-let pro = 0
-let sum = 0
-let k = 2
-let add = 2 * k
+const k = 2
 
-let arrd = []
-
-for (let i = 0; i < arrA.length; i++) {
-   pro = (arrA[i] * arrB[i])
-    sum += pro 
+const minimumSumProduct = (arrA, arrB, k) => {
+    let pro = 0
+    let sum = 0
+    let add = 2 * k
+    let arrd = []
+    for (let i = 0; i < arrA.length; i++) {
+       pro = (arrA[i] * arrB[i])
+        sum += pro 
+    }
+    for (let i = 0; i < arrA.length; i++) {
+        arrd.push(sum +(arrB[i] * add))
+        arrd.push(sum - (arrB[i] * add))
+    }
+    return Math.min(...arrd)
 }
 
-for (let i = 0; i < arrA.length; i++) {
-    arrd.push(sum +(arrB[i] * add))
-    arrd.push(sum - (arrB[i] * add))
-}
-
-
-let menor = Math.min(...arrd)
-
-console.log(menor)
+console.log(minimumSumProduct(arrA, arrB, k))
