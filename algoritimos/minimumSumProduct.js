e886f823a4320e01704d28fd989776d2b4b46002
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-16 11:07:06 -0300

    refactor

--- algoritimos/minimumSumProduct.js
+++ algoritimos/minimumSumProduct.js
@@ -4,25 +4,20 @@ let pro = 0
 let sum = 0
 let k = 2
 let add = 2 * k
-let add0 = (arrB[0] * add) 
-let add1 = (arrB[1] * add) 
-let add2 = (arrB[2] * add) 
+
+let arrd = []
 
 for (let i = 0; i < arrA.length; i++) {
    pro = (arrA[i] * arrB[i])
     sum += pro 
 }
 
-let arrC = []
+for (let i = 0; i < arrA.length; i++) {
+    arrd.push(sum +(arrB[i] * add))
+    arrd.push(sum - (arrB[i] * add))
+}
 
-arrC.push(sum + add0)
-arrC.push(sum + add1)
-arrC.push(sum + add2)
 
-arrC.push(sum - add0)
-arrC.push(sum - add1)
-arrC.push(sum - add2)
-
-let menor = Math.min(...arrC)
+let menor = Math.min(...arrd)
 
 console.log(menor)
