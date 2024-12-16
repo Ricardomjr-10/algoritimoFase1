0686f12685584fb471cba9c6ee6c3ce608849e97
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-16 09:47:42 -0300

    add k

--- algoritimos/minimumSumProduct.js
+++ algoritimos/minimumSumProduct.js
@@ -1,10 +1,11 @@
 const arrA = [1, 2, 3]
 const arrB = [4, 5, 6]
-let arrC = []
+let pro = 0
 let sum = 0
+let k = 2
 
 for (let i = 0; i < arrA.length; i++) {
-    arrC.push(arrA[i] * arrB[i])
-    sum += arrC[i]
+   pro = (arrA[i] * arrB[i])
+    sum += pro
 }
 console.log(sum)
