8b5920a89a3756df9efd20881db3ce8ba39dc809
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-17 14:55:08 -0300

    add instancias

--- algoritimos/jobSequenciProblem.js
+++ algoritimos/jobSequenciProblem.js
@@ -6,12 +6,14 @@ class Job {
     }
 }
 
-const job = new Job('a', 4, 20)
-const job1 = new Job('b', 1, 10)
-const job2 = new Job('c', 1, 40)
-const job3 = new Job('d', 1, 30)
+const jobs = [
+    new Job('a', 4, 20),
+    new Job('b', 1, 10),
+    new Job('c', 1, 40),
+    new Job('d', 1, 30)
+]
 
 
 
 
-console.log(job.deadline)
+console.log(jobs)
