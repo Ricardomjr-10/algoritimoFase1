8319ae8586d0ca29268e2691a441b1d87643c018
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-17 14:46:04 -0300

    add class

--- algoritimos/jobSequenciProblem.js
+++ algoritimos/jobSequenciProblem.js
@@ -5,3 +5,13 @@ class Job {
         this.profit = profit
     }
 }
+
+const job = new Job('a', 4, 20)
+const job1 = new Job('b', 1, 10)
+const job2 = new Job('c', 1, 40)
+const job3 = new Job('d', 1, 30)
+
+
+
+
+console.log(job.deadline)
