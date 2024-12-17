c2460d3052d54577e0173d8c50474509e21dad72
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-17 09:37:26 -0300

    testando function

---
+++ algoritimos/quickSort.js
@@ -0,0 +1,21 @@
+const quickSort = array => {
+    if (array.length <= 1) {
+        return array
+    }
+
+    let pivot = array[0]
+    let left = []
+    let rigth = []
+
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < pivot) {
+            left.push(array[i])
+        } else {
+            rigth.push(array[i])
+        }
+    }
+
+    return quickSort(left).concat(pivot).concat(quickSort(rigth))
+}
+
+console.log(quickSort([38, 27, 43, 10,20,7]))
