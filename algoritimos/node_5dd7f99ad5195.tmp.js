ddcc7ec573854d592782e4c5a6e963ae17a8452d
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-19 17:15:00 -0300

    add func minimizeCashFlow

--- algoritimos/node_5dd7f99ad5195.tmp.js
+++ algoritimos/node_5dd7f99ad5195.tmp.js
@@ -1,4 +1,29 @@
+let n = 3
 
+const getMin = arr => {
+    let minInd = 0
+    for (let i = 0; i < n; i++) {
+        if (arr[i] < arr[minInd]) {
+            minInd = i
+        }
+    }
+    return minInd
+}
+
+const getMax = arr => {
+    let maxInd = 0
+    for (let i = 0; i < n; i++) {
+        if (arr[i] > arr[maxInd]) {
+            maxInd = i
+        }
+
+    }
+    return maxInd
+}
+
+const minOf2 = (a, b) => {
+    return a < b ? a : b
+}
 
 const minimizeCashFlow = (graph) => {
     let mxCredit = -1,
