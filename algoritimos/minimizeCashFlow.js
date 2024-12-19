f11088803daee99fafd535ee831a020a25311b2d
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-19 17:05:25 -0300

    add function minOf2

--- algoritimos/minimizeCashFlow.js
+++ algoritimos/minimizeCashFlow.js
@@ -1,22 +1,27 @@
 let n = 3
 
 const getMin = arr => {
-    let min = 0
+    let minInd = 0
     for (let i = 0; i < n; i++) {
-        if (arr[i] < arr[min]) {
-            min = i
+        if (arr[i] < arr[minInd]) {
+            minInd = i
         }
     }
-    return min
+    return minInd
 }
 
 const getMax = arr => {
-    let max = 0
+    let maxInd = 0
     for (let i = 0; i < n; i++) {
-        if (arr[i] > arr[max]) {
-            max = i
+        if (arr[i] > arr[maxInd]) {
+            maxInd = i
         }
 
     }
-    return max
+    return maxInd
 }
+
+const minOf2 = (a, b) => {
+    return a < b ? a : b
+}
+
